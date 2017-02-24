c219790b466bf797923b18ce454d983fa7d66577
Mille <mille.bostrom@testura.net>
2017-02-24 07:36:24 +0100

    Wrote unit tests for string extensions

--- src/Testura.Code.CecilHelpers/Extensions/StringExtensions.cs
+++ src/Testura.Code.CecilHelpers/Extensions/StringExtensions.cs
@@ -4,11 +4,21 @@
     {
         public static string FirstLetterToLowerCase(this string value)
         {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
             return char.ToLowerInvariant(value[0]) + value.Substring(1);
         }
 
         public static string FirstLetterToUpperCase(this string value)
         {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
             return char.ToUpperInvariant(value[0]) + value.Substring(1);
         }
     }
