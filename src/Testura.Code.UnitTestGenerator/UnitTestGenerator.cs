6eba50ee3fd7ed7007aa0b173157873d1a45472b
Mille <mille.bostrom@testura.net>
2017-03-01 23:06:06 +0100

    Created new async method and updated nuget package

--- src/Testura.Code.UnitTestGenerator/UnitTestGenerator.cs
+++ src/Testura.Code.UnitTestGenerator/UnitTestGenerator.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Policy;
+using System.Threading.Tasks;
 using Testura.Code.UnitTestGenerator.Util;
 using Testura.Code.Util.AppDomains;
 
@@ -6,10 +11,26 @@ namespace Testura.Code.UnitTestGenerator
 {
     public class UnitTestGenerator
     {
+        public UnitTestGenerator()
+        {
+            ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            Permissions = new PermissionSet(PermissionState.Unrestricted);
+        }
+
         /// <summary>
         /// Gets or sets the application base for the app domain
         /// </summary>
         public string ApplicationBase { get; set; }
+
+        /// <summary>
+        /// Gets or sets the app domain evidence
+        /// </summary>
+        public Evidence Evidence { get; set; }
+
+        /// <summary>
+        /// Gets or sets the app domain permissions
+        /// </summary>
+        public PermissionSet Permissions { get; set; }
 
         /// <summary>
         /// Generate unit tests from an assembly in a different app domain
@@ -27,8 +48,21 @@ namespace Testura.Code.UnitTestGenerator
                 ["outputPath"] = outputDirectory
             };
 
-            var appDomainCodeGenerator = new AppDomainCodeGenerator { ApplicationBase = ApplicationBase};
+            var appDomainCodeGenerator = new AppDomainCodeGenerator { ApplicationBase = ApplicationBase, Evidence = Evidence, Permissions = Permissions};
             appDomainCodeGenerator.GenerateCode(assemblyPath, new UnitTestGeneratorProxy(), extraData);
         }
+
+        /// <summary>
+        /// Generate unit tests from an assembly in a different app domain async
+        /// </summary>
+        /// <param name="assemblyPath">Path to the assembly</param>
+        /// <param name="testFramework">Test framework to use when generating code</param>
+        /// <param name="mockFramework">Mock framework to use when generating code</param>
+        /// <param name="outputDirectory">The output path for the generated dll file</param>
+        public Task GenerateUnitTestsAsync(string assemblyPath, TestFrameworks testFramework, MockFrameworks mockFramework, string outputDirectory)
+        {
+            return Task.Run(() => GenerateUnitTests(assemblyPath, testFramework, mockFramework, outputDirectory));
+        }
+
     }
 }
